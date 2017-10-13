e7dd2ea164887ed47b22e5a8c0b2ec6dc74ad59c
Kevin Schaffter <kevinschaffter@gmail.com>
2017-10-13 12:16:56 -0400

    Add seller page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :listings
   get 'contact', to: 'pages#contact'
+  get "seller", to: 'listings#seller'
  
   root to: 'pages#home'
 
