fd51096bbfea9b00c0b7775122ace36adeabbe32
Kevin Schaffter <kevinschaffter@gmail.com>
2017-10-13 11:08:24 -0400

    associated users with listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,12 @@
 class Listing < ApplicationRecord
   has_attached_file :image, styles: { medium: "200x", thumb: "100x100" }, default_url: "thumb.jpg"
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+    validates :name, :description, :price, :category, :country, presence: true
+    validates :price, numericality: { greater_than: 0 }
+
+    validates_attachment_presence :image
+
+    belongs_to :user
+
 end
