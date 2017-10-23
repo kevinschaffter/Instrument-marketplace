7b8b9873efd8b5290ecb3b8ffd78a7fecb3e350e
Kevin Schaffter <kevinschaffter@gmail.com>
2017-10-23 09:53:57 -0400

    Updated styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171018213634) do
+ActiveRecord::Schema.define(version: 20171021184245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20171018213634) do
     t.text "description"
     t.integer "user_id"
     t.string "category_id"
+    t.string "condition"
   end
 
   create_table "personal_messages", force: :cascade do |t|
